613644639c32f1b0c60b8f04036864d19d52ccbd
str-leshs <0805eslee@gmail.com>
2024-07-13 12:44:16 +0900

    관리자 페이지 snb, 대시보드 레이아웃 구성.

--- evoproject/src/main/java/com/evo/evoproject/controller/admin/AdminController.java
+++ evoproject/src/main/java/com/evo/evoproject/controller/admin/AdminController.java
@@ -1,13 +1,24 @@
 package com.evo.evoproject.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
-    @GetMapping("admin")
+    @GetMapping("/admin")
     public String admin() {
-        return "admin";
+        return "admin/admin";
+    }
+
+    @GetMapping("/admin/manageOrder")
+    public String manageOrder() {
+        return "admin/manageOrder";
+    }
+
+    @GetMapping("/admin/manageBoard")
+    public String manageBoard() {
+        return "admin/manageBoard";
     }
 }
