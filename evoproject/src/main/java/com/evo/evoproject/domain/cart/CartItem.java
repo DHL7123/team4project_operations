bdb6a233881eae5ef5d8f809cba056c0cc1c6846
str-leshs <0805eslee@gmail.com>
2024-07-16 14:12:45 +0900

    장바구니 상품 이미지 추가 및 레이아웃 수정

--- evoproject/src/main/java/com/evo/evoproject/domain/cart/CartItem.java
+++ evoproject/src/main/java/com/evo/evoproject/domain/cart/CartItem.java
@@ -1,5 +1,6 @@
 package com.evo.evoproject.domain.cart;
 
+import com.evo.evoproject.domain.image.Image;
 import lombok.Data;
 
 @Data
@@ -8,7 +9,7 @@ public class CartItem {
     private String proName;
     private int proPrice;
     private int shipping;
-    private int soldout;
+
+    private Image mainImage;
 }
-//상품 수량 추가
-//soldout 삭제
+
