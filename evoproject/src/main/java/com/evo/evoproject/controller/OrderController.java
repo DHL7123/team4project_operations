584dfcac0d1d098b1956cddf2ec579ec45e75e04
TerryKim2 <137304324+TerryKim2@users.noreply.github.com>
2024-07-18 11:42:20 +0900

    S3 configuration만 추가하면 실행 가능하도록 수정.

--- evoproject/src/main/java/com/evo/evoproject/controller/OrderController.java
+++ evoproject/src/main/java/com/evo/evoproject/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
--- evoproject/src/main/java/com/evo/evoproject/controller/PaymentController.java
+++ evoproject/src/main/java/com/evo/evoproject/controller/PaymentController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PaymentController {
